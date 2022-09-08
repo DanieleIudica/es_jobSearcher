8ffe6079c12cd16d37cdba87dec4dfbeac1da8fe
DanieleIudica <daniele.iudica@hotmail.it~>
2022-09-08 16:40:26 +0200

    es_job_searcher

---
+++ src/components/Favourites.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import Job from "./Job";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export const Favourites = () => {
+  const favourites = useSelector((state) => state.favourites.content);
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Row>
+        <Col xs={2} className="mx-auto mt-4">
+          <Button variant="dark" onClick={() => navigate("/")}>
+            Back to Home
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10} className="mx-auto mb-5">
+          {favourites.map((jobData) => (
+            <Job key={jobData._id} data={jobData} />
+          ))}
+        </Col>
+      </Row>
+    </>
+  );
+};
---
+++ src/components/Job.jsx
@@ -0,0 +1,52 @@
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
+  const favourites = useSelector((state) => state.favourites.content);
+
+  return (
+    <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
+      <Col xs={1}>
+        {favourites.find((favourite) => data.company_name === favourite.company_name) ? (
+          <FaHeart
+            className="text-danger"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              console.log("ora faccio il dispatch di un'action");
+              dispatch({
+                type: "REMOVE_FROM_FAVOURITES",
+                payload: data._id,
+              });
+            }}
+          />
+        ) : (
+          <FaRegHeart
+            className="text-danger"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              console.log("ora faccio il dispatch di un'action");
+              dispatch({
+                type: "ADD_TO_FAVOURITES",
+                payload: data,
+              });
+            }}
+          />
+        )}
+      </Col>
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={8}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
